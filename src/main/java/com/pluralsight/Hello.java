68747f64683b621d3aefa1a65f3b46f3f41f46c2
beatrizjardim031 <beatrizreisjardim@gmail.com>
2026-04-13 12:55:49 -0500

    Initial commit

---
+++ src/main/java/com/pluralsight/Hello.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class Hello {
+// Exercise 1a-e
+
+    public static void sayHello() {
+        System.out.println ("Hello, World!");
+    }
+
+    public static void sayGoodbye() {
+        System.out.println("Goodbye!");
+    }
+}
