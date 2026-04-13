94a407c60108f7b517a6be2074caa05989082b9f
beatrizjardim031 <beatrizreisjardim@gmail.com>
2026-04-13 13:09:41 -0500

    Feat: Call displayMenu method in the main one

--- src/main/java/com/pluralsight/Menu.java
+++ src/main/java/com/pluralsight/Menu.java
@@ -4,6 +4,12 @@ public class Menu {
 //  Exercise 1 part 3
 
     public static void displayMenu(){
-        System.out.printf("=== MENU === %n 1. Coffee - $3.99 %n 2. Tea - $2.99 %n 3. Cookie - $1.99");
+        System.out.printf("%n=== MENU ===%n1. Coffee - $3.99%n2. Tea - $2.99%n3. Cookie - $1.99");
+    }
+
+    public static void main(String[] args) {
+        displayMenu();
+
+        displayMenu();
     }
 }
