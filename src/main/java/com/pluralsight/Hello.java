c37bd85c9b6e4d342670617d8c49014b9e12b4ea
beatrizjardim031 <beatrizreisjardim@gmail.com>
2026-04-13 13:01:48 -0500

    Feat: Call methods in the main methods

--- src/main/java/com/pluralsight/Hello.java
+++ src/main/java/com/pluralsight/Hello.java
@@ -1,8 +1,9 @@
 package com.pluralsight;
 
 public class Hello {
-// Exercise 1a-e
+//  Exercise 1 part 2 (a-e)
 
+//  In this exercise I'm defining methods, the main method is where I call the methods I defined
     public static void sayHello() {
         System.out.println ("Hello, World!");
     }
@@ -10,4 +11,15 @@ public class Hello {
     public static void sayGoodbye() {
         System.out.println("Goodbye!");
     }
+
+    public static void sayGoodMorning() {
+        System.out.println("Good Morning!");
+    }
+
+    public static void main(String[] args) {
+        sayHello();
+        sayGoodbye();
+        sayGoodMorning();
+    }
 }
+
