d39f50668f9f4666946e80df0bb6339a2342c95d
beatrizjardim031 <beatrizreisjardim@gmail.com>
2026-04-13 13:06:33 -0500

    Initial commit

---
+++ src/main/java/com/pluralsight/Menu.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class Menu {
+//  Exercise 1 part 3
+
+    public static void displayMenu(){
+        System.out.printf("=== MENU === %n 1. Coffee - $3.99 %n 2. Tea - $2.99 %n 3. Cookie - $1.99");
+    }
+}
